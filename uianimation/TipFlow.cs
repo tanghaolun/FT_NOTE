daa7766b2fceaad085e82dc14895a77ec9f5472e
tanghaolun <tanghaolun@users.noreply.github.com>
2019-12-02 17:30:06 +0800

    add some ui of animations

---
+++ uianimation/TipFlow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class TipFlow : MonoBehaviour
+{
+    float tiptime = 0;
+    void OnEnable()
+    {
+        tiptime = 0;
+    }
+
+    void Update()
+    {
+        tiptime += Time.deltaTime;
+        if (tiptime > 1.2f)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+}
---
+++ uianimation/UiFillAni.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiFillAni : MonoBehaviour
+{
+    public Image FillImage;
+
+    public float DelayTime;
+
+    public float DelayTimeMove;
+
+    public float FillDu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnEnable()
+    {
+        FillDu = 0;
+        DelayTimeMove = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DelayTimeMove += Time.deltaTime;
+        if (DelayTimeMove < DelayTime)
+        {
+            return;
+        }
+
+        if (FillDu < 1)
+        {
+            FillDu += Time.deltaTime*3f;
+            FillImage.fillAmount = FillDu;
+        }
+
+
+    }
+}
